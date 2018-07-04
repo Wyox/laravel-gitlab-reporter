0abf9a882c71a6a0cb488e62f2e64047481f04a4
Ivo de Bruijn <ivo@idobits.nl>
2018-07-04 12:47:14 +0200

    Indentation

--- src/GitlabReportService.php
+++ src/GitlabReportService.php
@@ -20,8 +20,8 @@ use Exception;
 
 class GitlabReportService
 {
-        private $client;
-        private $project_id;
+    private $client;
+    private $project_id;
 
     /**
      * GitlabReportService constructor.
@@ -31,11 +31,11 @@ class GitlabReportService
      */
 
     public function __construct($url, $token, $project_id)
-        {
-            $this->client = Client::create($url)->authenticate($token,Client::AUTH_URL_TOKEN);
-            $this->project_id = $project_id;
-            return $this;
-        }
+    {
+        $this->client = Client::create($url)->authenticate($token,Client::AUTH_URL_TOKEN);
+        $this->project_id = $project_id;
+        return $this;
+    }
 
 
     /**
@@ -44,30 +44,30 @@ class GitlabReportService
      */
     public function report(Exception $exception){
 
-            try {
+        try {
 
-                // Get current request
-                $request = $this->request();
+            // Get current request
+            $request = $this->request();
 
-                $report = new ErrorReport($exception, $request);
+            $report = new ErrorReport($exception, $request);
 
-                $project = new Project($this->project_id, $this->client);
+            $project = new Project($this->project_id, $this->client);
 
-                // Check if an issue exists with the same title and is currently open.
-                $issues = $project->issues(['search' => $report->title(), 'state' => 'opened']);
+            // Check if an issue exists with the same title and is currently open.
+            $issues = $project->issues(['search' => $report->title(), 'state' => 'opened']);
 
-                if (empty($issues)) {
-                    $issue = $project->createIssue($report->title(), [
-                        'description' => $report->description()
-                    ]);
-                }
-            } catch (Exception $exp){
-                // Only for testing
-                 //throw $exp;
+            if (empty($issues)) {
+                $issue = $project->createIssue($report->title(), [
+                    'description' => $report->description()
+                ]);
             }
+        } catch (Exception $exp){
+            // Only for testing
+            //throw $exp;
+        }
 
 
-            return;
+        return;
     }
 
 
@@ -76,7 +76,7 @@ class GitlabReportService
      * @return \Illuminate\Http\Request
      */
     private function request() {
-            return app(Request::class);
-        }
+        return app(Request::class);
+    }
 
 }
