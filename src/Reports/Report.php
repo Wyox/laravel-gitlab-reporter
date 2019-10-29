7bdfadd4a5912d5b244e253ab56df09ca1079ae7
Ivo de Bruijn <ik@ivodebruijn.nl>
2019-10-29 21:35:37 +0100

    Refactor a bunch of stuff

---
+++ src/Reports/Report.php
@@ -0,0 +1,60 @@
+<?php
+
+
+namespace Wyox\GitlabReport\Reports;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+
+abstract class Report
+{
+
+    protected $exception;
+    protected $request;
+
+    public function __construct(\Exception $exception, Request $request)
+    {
+        $this->exception = $exception;
+        $this->request = $request;
+    }
+
+    /**
+     * This returns a unique signature based on the exception, the query and input parameters
+     * @return string
+     */
+
+    public function signature()
+    {
+        // Signature should be unique to the error (ignore session for now)
+        // Signature should be unique to the error (ignore session for now)
+        $key = $this->message() . $this->exception->getFile() . $this->exception->getTraceAsString() . $this->exception->getCode();
+        // This might fail if it has complex objects
+        $key .= (new Collection($this->request->request->all()))->toJson();
+        $key .= (new Collection($this->request->query->all()))->toJson();
+
+        return hash('md5', $key);
+    }
+
+    /**
+     * Returns description "rendered"
+     * @return string
+     */
+    public function render()
+    {
+        // Limit string to 1048575 characters per gitlab api limit
+        return substr($this->description(),0, 1048575);
+    }
+
+    /**
+     * Generates a GitLab issue title
+     * @return string
+     */
+    public function title()
+    {
+        return substr("BUG: {$this->message()}", 0, 254);
+    }
+
+    abstract public function message();
+    abstract public function description();
+    abstract public function identifier();
+}
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -24,16 +24,20 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
         $this->mergeConfigFrom(__DIR__ . '/../config/gitlab-report.php', 'gitlab-report');
 
         $this->app->singleton(GitlabReportService::class, function ($app) {
+            $config = array_merge(
+                [
+                    'url' => null,
+                    'token' => null,
+                    'project_id' => null,
+                    'labels' => '',
+                    'ignore-exceptions' => [],
+                    'redacted-fields' => [],
+                    'debug' => false
+                ],
+                $app->make('config')->get('gitlab-report', [])
+            );
 
-            $config = $app->make('config');
-            $url = $config->get('gitlab-report.url');
-            $token = $config->get('gitlab-report.token');
-            $project_id = $config->get('gitlab-report.project_id');
-            $labels = $config->get('gitlab-report.labels');
-            $ignore = $config->get('gitlab-report.ignore-exceptions', []);
-            $redacted = $config->get('gitlab-report.redacted-fields', []);
-
-            return new GitlabReportService($url, $token, $project_id, $labels, $ignore, $redacted);
+            return new GitlabReportService($config);
         });
 
         $this->app->alias(GitlabReportService::class, 'gitlab.report');
